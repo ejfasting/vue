f4be2763019cda54b5881f9c216738b628b7688e
Eivind Fasting <eivind.fasting@superoffice.com>
2024-06-19 09:09:14 +0200

    updated gitlab

--- src/components/vite-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-declare module 'monaco-editor/esm/vs/editor/editor.worker' {
-  export function initialize(callback: (ctx: any, createData: any) => any): void
-}
