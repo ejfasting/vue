ea114f9b85bfb8adc2b66d2f9997521e679ffa86
Eivind Fasting <eivind.fasting@superoffice.com>
2024-06-19 11:07:56 +0200

    Deploy to gh-pages

--- src/components/monaco/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Uri, editor } from 'monaco-editor'
-
-export function getOrCreateModel(uri: Uri, lang: string | undefined, value: string) {
-  let model = editor.getModel(uri)
-  if (!model) {
-    model = editor.createModel(value, lang, uri)
-  }
-  return model
-}
